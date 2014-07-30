4b818b804e2e6742387e4d87d405d42101ec5604
thomaspringle <pringle.thomas@gmail.com>
2014-07-30 23:22:19 +0200

    Recurrence dialog

---
+++ src/com/android/calendar/Utils.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2006 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.calendar;
+
+import java.util.Calendar;
+import java.util.Locale;
+
+import android.content.Context;
+import android.text.format.Time;
+
+public class Utils {
+
+    public static int getFirstDayOfWeek(Context context) {
+
+        int startDay = Calendar.getInstance(Locale.getDefault()).getFirstDayOfWeek();
+
+        if (startDay == Calendar.SATURDAY) {
+            return Time.SATURDAY;
+        } else if (startDay == Calendar.MONDAY) {
+            return Time.MONDAY;
+        } else {
+            return Time.SUNDAY;
+        }
+    }
+
+}
